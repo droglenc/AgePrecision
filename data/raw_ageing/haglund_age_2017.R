a7453132195a2ae1f7d6795ff6e225aeac7f416f
Derek Ogle <droglenc@users.noreply.github.com>
2018-11-13 07:27:27 -0600

    Added tidied and analyzed Haglund data

---
+++ data/raw_ageing/haglund_age_2017.R
@@ -0,0 +1,39 @@
+#### SETUP #####################################################################
+cat("\014"); rm(list=ls())
+setwd(here::here())
+source("code/precisionData.R")
+
+nm <- "haglund_age_2017"       ## Name of study (actually name of file)
+df <- read.csv(paste0("data/raw_ageing/",nm,".csv"))
+str(df) 
+
+
+## !!! Copy the code below if more than one structure of comparisons
+
+#### XXXXXX ####################################################################
+species <- "Brown Trout"
+atype <- "between"  # possibly change to "within"
+strux <- "otoliths"         # Calcified strucure (e.g., scales, otolith, finray, spine)
+strux2 <- "sagittae"        # More about scturcture (e.g., dorsal, pectoral)
+proc <- "sectioned"          # Process info (e.g., sectioned, crackburn, whole)
+
+df1 <- df %>%       # Process the data to prepare for analysis
+  select(r1,r2,r3) %>%
+  filterD(complete.cases(.))
+ap1 <- agePrecision(~r1+r2+r3,data=df1)   ## include the variable names here
+pt1SD <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="SD")
+plot(pt1SD)
+summary(pt1SD,what="tests")
+
+pt1CV <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="CV")
+plot(pt1CV)
+summary(pt1CV,what="tests")
+
+res <- list(sum=pt1SD$sum,tests=rbind(pt1SD$tests,pt1CV$tests))
+saveRDS(res,paste0("data/results_precision/",nm,"_",species,"_",strux,
+                   ifelse(strux2=="","","_"),strux2,
+                   ifelse(proc=="","","_"),proc,".rds"))
