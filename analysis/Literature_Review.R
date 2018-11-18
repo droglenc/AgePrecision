1d1d47fa6cbd34b165c409ebca212e470823de78
Derek Ogle <droglenc@users.noreply.github.com>
2018-11-18 17:31:17 -0600

    Worked on cleaning the lit review database

--- analysis/Literature_Review.R
+++ analysis/Literature_Review.R
@@ -1,11 +1,39 @@
+cat("\014")
+setwd(here::here())
+
+library(dplyr)
+library(readxl)
+
 fn <- googledrive::as_id("https://docs.google.com/spreadsheets/d/1RY6DQyi-zCfg_BQ2l_cZRZC_fA6PI3zutFIMIvJfbw8/edit?ts=5bac4a12#gid=0")
 googledrive::drive_download(file=fn,path="data/Literature_Review",overwrite=TRUE)
 
-study <- readxl::read_excel("data/Literature_Review.xlsx",
-                            sheet="Study",na=c("","NA")) %>%
-  select(-notes,-minedrefs)
-str(study)
-res <- readxl::read_excel("data/Literature_Review.xlsx",
-                          sheet="Results",na=c("","NA")) %>%
-  select(-notes,-tech)
-str(res)
+## Make the overall database
+##   Read fish names
+fish <- read_excel("data/Literature_Review.xlsx",sheet="FishNames") %>%
+  select(-sciname)
+##   Read results and append on fish name info
+tmp <- read_excel("data/Literature_Review.xlsx",sheet="Results_Meta")
+res <- readxl::read_excel("data/Literature_Review.xlsx",sheet="Results",
+                          na=c("-",""),col_types=tmp$RType) %>%
+  select(-notes)
+res <- left_join(res,fish,by="species")
+##   Read study info and append on results
+tmp <- read_excel("data/Literature_Review.xlsx",sheet="Study_Meta")
+df <- read_excel("data/Literature_Review.xlsx",sheet="Study",
+                    na=c("-",""),col_types=tmp$RType) %>%
+  select(-notes,-OrigFile)
+df <- left_join(df,res,by="studyID") %>%
+  filter(USE=="yes") %>%
+  select(-USE) %>%
+  mutate(structure=factor(structure,levels=c("otoliths","spines","finrays",
+                                             "scales","vertebrae","thorns",
+                                             "other")),
+         structure2=factor(structure2,levels=c("sagittae","lapillae","asterisci","statoliths",
+                                               "anal","dorsal","pectoral","pelvic","caudal",
+                                               "branchiostegal rays","gular plate","metapterygoid",
+                                               "pectoral articulating process","scute",
+                                               "sphenoid")))
+##   Clean up
+rm(tmp,fish,res,fn)
+
+
---
+++ analysis/Literature_Review_CLEANER.R
@@ -0,0 +1,45 @@
+## Scripts to help "clean" the literature review data
+cat("\014")
+setwd(here::here())
+library(FSA)
+
+## Create the literature review database
+source("analysis/Literature_Review.R")
+str(df)
+
+# check for non-standard entries
+xtabs(~structure,data=df)
+xtabs(~structure2+structure,data=df)
+xtabs(~process+structure,data=df)
+xtabs(~type,data=df)
+xtabs(~checkbias,data=df)
+xtabs(~biasmethod+checkbias,data=df)  # should not be a "no" column
+
+# check for NAs or odd values
+Summarize(~R,data=df,digits=1)
+Summarize(~n,data=df,digits=1)
+
+
+# Should be a straightline with a slope of sqrt(2) for all R=2 comparisons
+plot(APE~ACV,data=df,pch=21,bg=ifelse(R==2,"green","red"))
+abline(a=0,b=1/sqrt(2),col="blue")
+plot(APE~ACV,data=df,pch=21,bg=ifelse(R==2,"green","red"),xlim=c(0,20),ylim=c(0,20))
+abline(a=0,b=1/sqrt(2),col="blue")
+
+
+plot(I(ACV/APE)~ACV,data=df,pch=21,bg=ifelse(R==2,"green","red"),ylim=c(1.1,1.6))
+abline(h=sqrt(2),col="blue")
+
+# use this to find off values
+#with(df,identify(ACV,ACV/APE))
+#as.data.frame(df)[c(116,146,310,337),
+#                  c("studyID","species","structure","process","APE","ACV")]
+
+plot(PA0~ACV,data=df,pch=21,bg=ifelse(R==2,"green","red"))
+plot(PA1~ACV,data=df,pch=21,bg=ifelse(R==2,"green","red"))
+plot(PA1~PA0,data=df,pch=21,bg=ifelse(R==2,"green","red"))
+
+hist(~ACV,data=df,w=2)
+hist(ACV~class,data=df,w=2)
+boxplot(ACV~class,data=df)
+Summarize(ACV~class,data=df)
