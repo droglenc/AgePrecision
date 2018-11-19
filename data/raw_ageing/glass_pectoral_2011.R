ac77e9969443cb2af464f271043728cdb77da6f5
Derek Ogle <droglenc@users.noreply.github.com>
2018-11-19 13:26:59 -0600

    Added Glass 2011 data

---
+++ data/raw_ageing/glass_pectoral_2011.R
@@ -0,0 +1,101 @@
+#### SETUP #####################################################################
+cat("\014"); rm(list=ls())
+setwd(here::here())
+source("code/precisionData.R")
+
+nm <- "glass_pectoral_2011"       ## Name of study (actually name of file)
+df <- read.csv(paste0("data/raw_ageing/",nm,".csv"))
+str(df) 
+
+#### XXXXXX ####################################################################
+species <- "Spotted Gar"
+atype <- "between"
+strux <- "finrays"
+strux2 <- "pectoral"
+proc <- "sectioned"
+extra_suffix <- ""
+
+df1 <- df %>%
+  select(contains("finrays")) %>%
+  filterD(complete.cases(.))
+
+ap1 <- agePrecision(~finrays_R1+finrays_R2,data=df1)
+pt1SD <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="SD")
+plot(pt1SD)
+summary(pt1SD,what="tests")
+
+pt1CV <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="CV")
+plot(pt1CV)
+summary(pt1CV,what="tests")
+
+res <- list(sum=pt1SD$sum,tests=rbind(pt1SD$tests,pt1CV$tests))
+saveRDS(res,paste0("data/results_precision/",nm,"_",species,"_",strux,
+                   ifelse(strux2=="","","_"),strux2,
+                   ifelse(proc=="","","_"),proc,
+                   ifelse(extra_suffix=="","","_"),extra_suffix,".rds"))
+
+#### XXXXXX ####################################################################
+species <- "Spotted Gar"
+atype <- "between"
+strux <- "other"
+strux2 <- "branchiostegal rays"
+proc <- "whole"
+extra_suffix <- ""
+
+df1 <- df %>%
+  select(contains("branchio")) %>%
+  filterD(complete.cases(.))
+
+ap1 <- agePrecision(~branchio_R1+branchio_R2,data=df1)
+pt1SD <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="SD")
+plot(pt1SD)
+summary(pt1SD,what="tests")
+
+pt1CV <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="CV")
+plot(pt1CV)
+summary(pt1CV,what="tests")
+
+res <- list(sum=pt1SD$sum,tests=rbind(pt1SD$tests,pt1CV$tests))
+saveRDS(res,paste0("data/results_precision/",nm,"_",species,"_",strux,
+                   ifelse(strux2=="","","_"),strux2,
+                   ifelse(proc=="","","_"),proc,
+                   ifelse(extra_suffix=="","","_"),extra_suffix,".rds"))
+
+#### XXXXXX ####################################################################
+species <- "Spotted Gar"
+atype <- "between"
+strux <- "otoliths"
+strux2 <- ""
+proc <- "ground"
+extra_suffix <- ""
+
+df1 <- df %>%
+  select(contains("otoliths")) %>%
+  filterD(complete.cases(.))
+
+ap1 <- agePrecision(~otoliths_R1+otoliths_R2,data=df1)
+pt1SD <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="SD")
+plot(pt1SD)
+summary(pt1SD,what="tests")
+
+pt1CV <- precisionData(ap1,studyID=nm,species=species,
+                       structure=strux,structure2=strux2,process=proc,
+                       type=atype,var="CV")
+plot(pt1CV)
+summary(pt1CV,what="tests")
+
+res <- list(sum=pt1SD$sum,tests=rbind(pt1SD$tests,pt1CV$tests))
+saveRDS(res,paste0("data/results_precision/",nm,"_",species,"_",strux,
+                   ifelse(strux2=="","","_"),strux2,
+                   ifelse(proc=="","","_"),proc,
+                   ifelse(extra_suffix=="","","_"),extra_suffix,".rds"))
